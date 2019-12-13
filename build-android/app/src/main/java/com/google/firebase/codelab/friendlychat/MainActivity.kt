bb1ee92eef85d013f54d9f0e419b0a8f16433f9d
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-12-13 22:34:10 +0900

    Refactoring

--- build-android/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
+++ build-android/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
@@ -119,11 +119,7 @@ class MainActivity : AppCompatActivity(), OnConnectionFailedListener {
         messageEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
             override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-                if (charSequence.toString().trim { it <= ' ' }.length > 0) {
-                    sendButton.isEnabled = true
-                } else {
-                    sendButton.isEnabled = false
-                }
+                sendButton.isEnabled = charSequence.toString().trim { it <= ' ' }.isNotEmpty()
             }
 
             override fun afterTextChanged(editable: Editable) {}
