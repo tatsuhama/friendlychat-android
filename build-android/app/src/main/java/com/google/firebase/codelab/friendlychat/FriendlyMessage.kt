9f6a37598c04e1d5e47ca2592f6103f3fd689e8a
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-12-13 22:31:17 +0900

    Convert to Kotlin

---
+++ build-android/app/src/main/java/com/google/firebase/codelab/friendlychat/FriendlyMessage.kt
@@ -0,0 +1,36 @@
+/**
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.firebase.codelab.friendlychat
+
+class FriendlyMessage {
+    var id: String? = null
+    var text: String? = null
+    var name = ""
+    var photoUrl: String? = null
+    var imageUrl: String? = null
+
+    constructor() {}
+    constructor(text: String?, name: String, photoUrl: String?, imageUrl: String?) {
+        this.text = text
+        this.name = name
+        this.photoUrl = photoUrl
+        this.imageUrl = imageUrl
+    }
+
+}
