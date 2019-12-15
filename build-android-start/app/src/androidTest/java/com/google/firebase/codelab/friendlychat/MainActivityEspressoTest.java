d8483d42f122148fd94f452b53f60ee9bb16da52
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-12-15 10:08:17 +0900

    fix build error

--- build-android-start/app/src/androidTest/java/com/google/firebase/codelab/friendlychat/MainActivityEspressoTest.java
+++ build-android-start/app/src/androidTest/java/com/google/firebase/codelab/friendlychat/MainActivityEspressoTest.java
@@ -17,17 +17,17 @@
 package com.google.firebase.codelab.friendlychat;
 
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.Espresso.onView;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -36,6 +36,4 @@ public class MainActivityEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
-
-    // Add instrumentation test here
 }
